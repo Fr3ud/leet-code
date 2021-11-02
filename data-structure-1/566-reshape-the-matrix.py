f6f088372c63b3d19cba47e4a548e72a08efc65c
Fr3ud <oo.pavelko@gmail.com>
2021-11-02 21:46:23 +0200

    ✅ 566. Reshape the Matrix

---
+++ data-structure-1/566-reshape-the-matrix.py
@@ -0,0 +1,18 @@
+# 566. Reshape the Matrix
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        N, M = len(mat[0]), len(mat)
+        T = r * c
+
+        if N * M != T: return mat
+
+        result = [[0 for _ in range(c)] for _ in range(r)]
+        k = 0
+
+        for i in range(M):
+          for j in range(N):
+            result[k // c][k % c] = mat[i][j]
+            k += 1
+
+        return result
