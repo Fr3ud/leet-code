cd78dcf2b6bcd475e658586af3157c0f61700f85
Fr3ud <oo.pavelko@gmail.com>
2021-11-03 22:03:46 +0200

    ✅ 74. Search a 2D Matrix

---
+++ data-structure-1/74-search-a-2d-matrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        top, bottom = 0, ROWS - 1
+        
+        while top <= bottom:
+            row = (top + bottom) // 2
+            
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bottom = row - 1
+            else:
+                break
+                
+        if not (top <= bottom):
+            return False
+        
+        row = (top + bottom) // 2
+        l, r = 0, COLS - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        
+        return False
