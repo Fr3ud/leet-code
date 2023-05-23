a82231b3bb3cd8e5e3f1aaab689ba0e95475620a
Alex Pavelko <oo.pavelko@gmail.com>
2023-05-23 10:14:09 +0200

    ✅ 2620. Counter

---
+++ 30-days-of-lc-javascript-challenge/2-2620-counter.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+const createCounter = n => () => n++
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
